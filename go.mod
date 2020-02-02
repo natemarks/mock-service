f66f0d58b914e7cf7d22fb550659bb275898ed80
Nate Marks <nmarks@imprivata.com>
2020-02-02 12:12:54 -0500

    heartbeat  is correctly refusing during shutdown

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gin-gonic/gin v1.5.0 // indirect
 	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/sirupsen/logrus v1.4.2
+	github.com/toorop/gin-logrus v0.0.0-20190701131413-6c374ad36b67 // indirect
 )
--- mock-service.go
+++ mock-service.go
@@ -1,9 +1,13 @@
 package main
 
+// example from
+// https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/server.go
+
 import (
 	"context"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"github.com/toorop/gin-logrus"
 	"net/http"
 	"os"
 	"os/signal"
@@ -23,10 +27,31 @@ func WaitResponse(w string) string {
 }
 
 func main() {
+	grace, err := time.ParseDuration(os.Getenv("SERVICE_GRACEFUL_SHUTDOWN_TIMEOUT"))
+	if err != nil {
+		grace, _ = time.ParseDuration("5000ms")
+	}
+
+	logger := log.New()
+	formatter := &log.TextFormatter{
+		FullTimestamp: true,
+	}
+	logger.SetFormatter(formatter)
+
+	// Only log the info severity or above.
+	logger.SetLevel(log.InfoLevel)
+
+	// I tried to move this to init() but it doesn't work there
+	log.SetOutput(os.Stdout)
 	router := gin.Default()
+	router.Use(ginlogrus.Logger(logger), gin.Recovery())
 	router.GET("/", func(c *gin.Context) {
 		q := c.Request.URL.Query()
 		c.String(http.StatusOK, WaitResponse(fmt.Sprint(q["wait"][0])))
+	})
+
+	router.GET("/heartbeat", func(c *gin.Context) {
+		c.Data(200, "text/plain", []byte("."))
 	})
 
 	srv := &http.Server{
@@ -51,7 +76,7 @@ func main() {
 	<-quit
 	log.Println("Shutdown Server ...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), grace)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal("Server Shutdown: ", err)
