0808e506c1efd5a0443aa5e2b018b12d2065837e
Nate Marks <nmarks@imprivata.com>
2020-02-02 10:29:11 -0500

    seems to be wokring with gin

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/natemarks/mock-service
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.5.0 // indirect
 	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/sirupsen/logrus v1.4.2
 )
--- mock-service.go
+++ mock-service.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	gin "github.com/gin-gonic/gin"
 )
 
 func WaitResponse(w string) string {
@@ -19,39 +21,41 @@ func WaitResponse(w string) string {
 		return "Invalid wait parameter example 500ms"
 	}
 }
-func TestEndpoint(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	q := r.URL.Query()
-	_, _ = w.Write([]byte(WaitResponse(fmt.Sprint(q["wait"][0]))))
-}
 
 func main() {
-	m := http.NewServeMux()
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		q := c.Request.URL.Query()
+		c.String(http.StatusOK, WaitResponse(fmt.Sprint(q["wait"][0])))
+	})
 
-	srv := http.Server{Addr: ":8080", Handler: m}
-	m.HandleFunc("/", TestEndpoint)
-
-	done := make(chan os.Signal, 1)
-	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
 
 	go func() {
+		// service connections
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
-	log.Print("Server Started")
 
-	<-done
-	log.Print("Server Stopped")
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer func() {
-		// extra handling here
-		cancel()
-	}()
-
+	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server Shutdown Failed:%+v", err)
+		log.Fatal("Server Shutdown: ", err)
 	}
-	log.Print("Server Exited Properly")
+
+	log.Println("Server exiting")
 }
